70aae5fe62dd61ce6c42e2c3b001aace6eabb62a
kimmjae99 <kimmjae99@gmail.com>
2024-05-08 23:50:11 +0900

    build: prettier 설정 및 eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,71 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true, node: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-    ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": [
-            "warn",
-            { allowConstantExport: true },
-        ],
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "import",
+    "simple-import-sort",
+    "react-refresh",
+    "@typescript-eslint",
+    "prettier",
+    "jsx-a11y",
+  ],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    // react
+    "react/prop-types": 0,
+    "react-hooks/rules-of-hooks": "error",
+    // import
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [["builtin", "external"], "internal", ["parent", "sibling"], "index"],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "internal",
+            position: "after",
+          },
+        ],
+      },
+    ],
+    // prettier
+    "prettier/prettier": "warn",
+    // rules
+    "no-console": [
+      "error",
+      {
+        allow: ["error", "warn"],
+      },
+    ],
+  },
 };
--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "header-max-length": [2, "always", 100], // commit 메시지 100자 이내 작성
-        "subject-case": [0], // commit subject 규칙 비활성화
-        "subject-empty": [2, "never"], // commit subject 필수 작성
-        "subject-full-stop": [2, "never", "."], // commit subject 마지막 .으로 마무리 X
-        "type-case": [2, "always", "lower-case"], // commit 타입 소문자
-        "type-empty": [2, "never"], // commit 타입 필수 작성
-        "type-enum": [
-            2,
-            "always",
-            [
-                "feat",
-                "refactor",
-                "modify",
-                "fix",
-                "style",
-                "merge",
-                "build",
-                "docs",
-                "perf",
-                "comment",
-                "rename",
-                "cleanup",
-            ],
-        ], // commit 타입 종류
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100], // commit 메시지 100자 이내 작성
+    "subject-case": [0], // commit subject 규칙 비활성화
+    "subject-empty": [2, "never"], // commit subject 필수 작성
+    "subject-full-stop": [2, "never", "."], // commit subject 마지막 .으로 마무리 X
+    "type-case": [2, "always", "lower-case"], // commit 타입 소문자
+    "type-empty": [2, "never"], // commit 타입 필수 작성
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "refactor",
+        "modify",
+        "fix",
+        "style",
+        "merge",
+        "build",
+        "docs",
+        "perf",
+        "comment",
+        "rename",
+        "cleanup",
+      ],
+    ], // commit 타입 종류
+  },
 };
