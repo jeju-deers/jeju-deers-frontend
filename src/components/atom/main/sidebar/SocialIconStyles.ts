78c2d9da0d74fb6913311d0a91f6fedfcc4cccbc
kimmjae99 <kimmjae99@gmail.com>
2024-05-16 14:52:43 +0900

    style: 소셜아이콘 hover시 색상 변경

--- src/components/atom/main/sidebar/SocialIconStyles.ts
+++ src/components/atom/main/sidebar/SocialIconStyles.ts
@@ -5,4 +5,9 @@ export const SocialIcon = styled.img`
   ${tw`
     w-7 h-7
   `}
+
+  &:hover {
+    filter: invert(61%) sepia(62%) saturate(1783%) hue-rotate(190deg) brightness(101%)
+      contrast(101%);
+  }
 `;
