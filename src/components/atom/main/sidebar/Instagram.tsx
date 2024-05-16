cfa0d42902daa5ef77c02deb5e0adcdae563e26c
kimmjae99 <kimmjae99@gmail.com>
2024-05-16 14:51:58 +0900

    style: 소셜 링크 추가

--- src/components/atom/main/sidebar/Instagram.tsx
+++ src/components/atom/main/sidebar/Instagram.tsx
@@ -1,6 +1,10 @@
 import { SocialIcon } from "./SocialIconStyles";
 import instagram from "../../../../assets/images/instagram_logo.svg";
 
-const Instagram = () => <SocialIcon src={instagram} alt="Instagram" />;
+const Instagram = () => (
+  <a href="https://www.instagram.com/jeju_deers_football/">
+    <SocialIcon src={instagram} alt="Instagram" />
+  </a>
+);
 
 export default Instagram;
--- src/components/atom/main/sidebar/Youtube.tsx
+++ src/components/atom/main/sidebar/Youtube.tsx
@@ -1,6 +1,10 @@
 import { SocialIcon } from "./SocialIconStyles";
 import youtube from "../../../../assets/images/youtube_logo.svg";
 
-const Youtube = () => <SocialIcon src={youtube} alt="Youtube" />;
+const Youtube = () => (
+  <a href="https://www.youtube.com/">
+    <SocialIcon src={youtube} alt="Youtube" />
+  </a>
+);
 
 export default Youtube;
