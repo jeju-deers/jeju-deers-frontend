b8a024809667d7c6963840d4b411a4eeed6271fa
kimmjae99 <kimmjae99@gmail.com>
2024-05-21 23:36:50 +0900

    rename: MainPage.tsx 내의 MainBody 컴포넌트 명 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,12 @@
+import MainBody from "../components/organism/main/MainBody";
 import Sidebar from "../components/organism/main/SideBar";
-import Main from "../components/organism/main/Main";
 import { MainPageLayout } from "./MainPageStyles";
 
 const MainPage = () => {
   return (
     <MainPageLayout>
       <Sidebar />
-      <Main />
+      <MainBody />
     </MainPageLayout>
   );
 };
