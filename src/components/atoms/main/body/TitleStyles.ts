08b70426b34d5c298676adec6e097fe205441104
kimmjae99 <kimmjae99@gmail.com>
2024-05-30 16:42:51 +0900

    style: Title 스타일 변경

--- src/components/atoms/main/body/TitleStyles.ts
+++ src/components/atoms/main/body/TitleStyles.ts
@@ -5,7 +5,7 @@ export const TitleBox = styled.div`
   ${tw`
     flex
     flex-col
-    gap-1
+    gap-3
     font-bold
     italic
     text-6xl
@@ -15,11 +15,12 @@ export const TitleBox = styled.div`
   p:first-child {
     ${tw`
       text-white
-      pb-2
     `}
   }
 
   p:nth-child(2) {
-    ${tw`text-[#69A6FF]`}
+    ${tw`
+      text-[#69A6FF]
+    `}
   }
 `;
