27c0d72b343f271917567991374caf6518b1b149
kimmjae99 <kimmjae99@gmail.com>
2024-06-04 17:19:58 +0900

    refactor: router 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -27,20 +27,20 @@ function App() {
         <Route path="/club/about" element={<AboutPage />}></Route>
         <Route path="/club/organization" element={<OrganizationPage />}></Route>
         <Route path="/club/roaster" element={<RoasterPage />}></Route>
-        <Route path="/club/coachstaff" element={<CoachStaffPage />}></Route>
+        <Route path="/club/member" element={<CoachStaffPage />}></Route>
         <Route path="/club/sponsor" element={<SponsorPage />}></Route>
 
         <Route path="/news/schedule" element={<SchedulePage />}></Route>
         <Route path="/news/news" element={<NewsPage />}></Route>
 
-        <Route path="/teamroom/practiceschedule" element={<PracticeSchedulePage />}></Route>
-        <Route path="/teamroom/teamboard" element={<TeamBoardPage />}></Route>
-        <Route path="/teamroom/coachboard" element={<CoachBoardPage />}></Route>
-        <Route path="/teamroom/staffboard" element={<StaffBoardPage />}></Route>
+        <Route path="/teamroom/practice_schedule" element={<PracticeSchedulePage />}></Route>
+        <Route path="/teamroom/team_board" element={<TeamBoardPage />}></Route>
+        <Route path="/teamroom/coach_board" element={<CoachBoardPage />}></Route>
+        <Route path="/teamroom/staff_board" element={<StaffBoardPage />}></Route>
         <Route path="/teamroom/playbook" element={<PlayBookPage />}></Route>
-        <Route path="/teamroom/membershipfee" element={<MembershipFeePage />}></Route>
+        <Route path="/teamroom/membership_fee" element={<MembershipFeePage />}></Route>
 
-        <Route path="/community/fanboard" element={<FanBoardPage />}></Route>
+        <Route path="/community/fan_board" element={<FanBoardPage />}></Route>
         <Route path="/community/media" element={<MediaPage />}></Route>
         <Route path="/community/support" element={<SupportPage />}></Route>
 
