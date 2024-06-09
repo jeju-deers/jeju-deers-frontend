93b39b4714a86c9d1495b2df038bf00161c7a285
HY219 <jhy_0219@daum.net>
2024-06-10 04:36:59 +0900

    style: SignUp template UI 구현

--- src/components/templates/signUp/SignUp.tsx
+++ src/components/templates/signUp/SignUp.tsx
@@ -1,7 +1,43 @@
-import { SignUpLayout } from "~/components/templates/signUp/SignUpStyles";
+import SubmitButton from "~/components/atoms/signUp/body/SubmitButton";
+import PrivacyPolicyField from "~/components/molecules/signUp/body/PrivacyPolicyField";
+import SignUpPurposeField from "~/components/molecules/signUp/body/SignUpPurposeField";
+import BasicItemInputField from "~/components/organisms/signUp/body/BasicItemInputField";
+import CoachesStaffItemInputField from "~/components/organisms/signUp/body/CoachesStaffItemInputField";
+import RosterItemInputField from "~/components/organisms/signUp/body/RosterItemInputField";
+import Header from "~/components/organisms/signUp/header/Header";
+import {
+  BasicItemInputFieldWrap,
+  OptionItemInputFieldWrap,
+  PrivacyPolicyFieldWrap,
+  SignUpForm,
+  SignUpLayout,
+  SignUpPurposeFieldWrap,
+  SubmitButtonWrap,
+} from "~/components/templates/signUp/SignUpStyles";
 
 const SignUp = () => {
-  return <SignUpLayout />;
+  return (
+    <SignUpLayout>
+      <Header />
+      <SignUpForm id="signUpSubmit">
+        <SignUpPurposeFieldWrap>
+          <SignUpPurposeField />
+        </SignUpPurposeFieldWrap>
+        <BasicItemInputFieldWrap>
+          <BasicItemInputField />
+        </BasicItemInputFieldWrap>
+        <OptionItemInputFieldWrap>
+          <RosterItemInputField />
+        </OptionItemInputFieldWrap>
+        <PrivacyPolicyFieldWrap>
+          <PrivacyPolicyField />
+        </PrivacyPolicyFieldWrap>
+        <SubmitButtonWrap>
+          <SubmitButton text="회원가입" formId="signUpSubmit" />
+        </SubmitButtonWrap>
+      </SignUpForm>
+    </SignUpLayout>
+  );
 };
 
 export default SignUp;
--- src/components/templates/signUp/SignUpStyles.tsx
+++ src/components/templates/signUp/SignUpStyles.tsx
@@ -3,6 +3,50 @@ import tw from "twin.macro";
 
 export const SignUpLayout = styled.div`
   ${tw`
+  
+  `}
+`;
 
+export const SignUpForm = styled.form`
+  ${tw`
+    flex
+    flex-col
+    items-center
+  `}
+`;
+
+export const SignUpPurposeFieldWrap = styled.div`
+  ${tw`
+    w-[930px]
+    py-[44px]
+  `}
+`;
+
+export const BasicItemInputFieldWrap = styled.div`
+  ${tw`
+    py-[44px]
+    border-t-[1px]
+    border-[#C8C8C8]
+  `}
+`;
+
+export const OptionItemInputFieldWrap = styled.div`
+  ${tw`
+    pt-[44px]
+    border-t-[1px]
+    border-[#C8C8C8]
+  `}
+`;
+
+export const PrivacyPolicyFieldWrap = styled.div`
+  ${tw`
+    pt-[28px]
+  `}
+`;
+
+export const SubmitButtonWrap = styled.div`
+  ${tw`
+    pt-[99px]
+    pb-[180px]
   `}
 `;
