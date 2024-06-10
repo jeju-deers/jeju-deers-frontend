b910dc816b30617584a967ef10fb9fb91e82ea45
HY219 <jhy_0219@daum.net>
2024-06-10 15:04:33 +0900

    refactor: 가입목적 radio버튼의 id를 이용하여 일치하는 inputField출력 & radio버튼이 처음에 선택된 상태로 출력

--- src/components/molecules/signUp/body/SignUpPurposeField.tsx
+++ src/components/molecules/signUp/body/SignUpPurposeField.tsx
@@ -8,10 +8,11 @@ import EssentialMark from "~/components/atoms/signUp/body/EssentialMark";
 import ItemRadio from "~/components/atoms/signUp/body/ItemRadio";
 
 interface Props {
+  selectedOption: string;
   onClick?: (element: any) => void;
 }
 
-const SignUpPurposeField = ({ onClick }: Props) => {
+const SignUpPurposeField = ({ selectedOption, onClick }: Props) => {
   return (
     <SignUpPurposeFieldBox>
       <SignUpPurposeLabelBox>
@@ -19,14 +20,27 @@ const SignUpPurposeField = ({ onClick }: Props) => {
         <ItemLabel id="signUpPurpose" text="가입 목적" />
       </SignUpPurposeLabelBox>
       <SignUpPurposeRadioBox>
-        <ItemRadio radioTitle="signUpPurpose" id="roaster" text="YB / OB 선수" onClick={onClick} />
+        <ItemRadio
+          radioTitle="signUpPurpose"
+          id="roster"
+          text="YB / OB 선수"
+          checked={selectedOption === "roster"}
+          onClick={onClick}
+        />
         <ItemRadio
           radioTitle="signUpPurpose"
           id="coachesStaff"
           text="코치 및 스탭"
+          checked={selectedOption === "coachesStaff"}
           onClick={onClick}
         />
-        <ItemRadio radioTitle="signUpPurpose" id="outsider" text="외부인" onClick={onClick} />
+        <ItemRadio
+          radioTitle="signUpPurpose"
+          id="outsider"
+          text="외부인"
+          checked={selectedOption === "outsider"}
+          onClick={onClick}
+        />
       </SignUpPurposeRadioBox>
     </SignUpPurposeFieldBox>
   );
--- src/components/templates/signUp/SignUp.tsx
+++ src/components/templates/signUp/SignUp.tsx
@@ -14,25 +14,34 @@ import {
 } from "~/components/templates/signUp/SignUpStyles";
 
 const SignUp = () => {
-  const [optionInputField, setOptionInputField] = useState(<RosterItemsInputField />);
-  const handleChange = (element: any) => {
-    if (element.target.id === "roster") {
-      setOptionInputField(<RosterItemsInputField />);
-    } else if (element.target.id === "coachesStaff") {
-      setOptionInputField(<CoachesStaffItemsInputField />);
-    } else if (element.target.id === "outsider") {
-      setOptionInputField(<></>);
+  const [selectedOption, setSelectedOption] = useState("roster");
+
+  const handleClick = (event: any) => {
+    const selectedId = event.target.id;
+    setSelectedOption(selectedId);
+  };
+
+  const getOptionInputField = () => {
+    if (selectedOption === "roster") {
+      return <RosterItemsInputField />;
+    }
+    if (selectedOption === "coachesStaff") {
+      return <CoachesStaffItemsInputField />;
+    }
+    if (selectedOption === "outsider") {
+      return <></>;
     }
   };
+
   return (
     <SignUpLayout>
       <Header />
       <SignUpForm id="signUpSubmit">
         <SignUpPurposeFieldWrap>
-          <SignUpPurposeField onClick={handleChange} />
+          <SignUpPurposeField selectedOption={selectedOption} onClick={handleClick} />
         </SignUpPurposeFieldWrap>
         <BasicItemsInputField />
-        <>{optionInputField}</>
+        <>{getOptionInputField()}</>
         <PrivacyPolicyFieldWrap>
           <PrivacyPolicyField />
         </PrivacyPolicyFieldWrap>
