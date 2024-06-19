eeee4c62d3876747b085e44ddbbcaa0a342fd212
HY219 <jhy_0219@daum.net>
2024-06-19 16:10:13 +0900

    build: axios 설치 및 axios instance 생성

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+});
