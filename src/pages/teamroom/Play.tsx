59586d1886ad6b8f544dfd6d69884668eae58954
kimmjae99 <kimmjae99@gmail.com>
2024-06-20 21:15:02 +0900

    rename: PlaybookPage 이름에 대한 에러 발견

--- src/pages/teamroom/Play.tsx
+++ src/pages/teamroom/Play.tsx
@@ -1,4 +1,4 @@
-import { PlaybookPageLayout } from "./PlaybookPageStyles";
+import { PlaybookPageLayout } from "./PlayStyles";
 
 const PlaybookPage = () => {
   return <PlaybookPageLayout>PlayBook 페이지입니다.</PlaybookPageLayout>;
