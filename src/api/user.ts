0b80ebd55ec0a53fd85b775a5921b7c431997d20
HY219 <jhy_0219@daum.net>
2024-07-01 06:46:05 +0900

    feat: 회원가입 요청 useMutation hook 정의 및 params type 정의

---
+++ src/api/user.ts
@@ -0,0 +1,44 @@
+import { instance } from ".";
+
+interface PostSignUpParams {
+  //   userType: "external" | "player" | "coach";
+  userType: string;
+  userId: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  nickname?: string;
+  email: string;
+  school?: string;
+  studentId?: string;
+  positions?: string;
+  backNumber?: string;
+}
+
+export const postSignUp = ({
+  userType,
+  userId,
+  password,
+  passwordConfirm,
+  name,
+  nickname,
+  email,
+  school,
+  studentId,
+  positions,
+  backNumber,
+}: PostSignUpParams) => {
+  return instance.post("/signup", {
+    userType,
+    userId,
+    password,
+    passwordConfirm,
+    name,
+    nickname,
+    email,
+    school,
+    studentId,
+    positions,
+    backNumber,
+  });
+};
