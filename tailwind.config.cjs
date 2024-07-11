9ca52affe0bc6460eb1cc7f66dfcb5cd703b7389
kimmjae99 <kimmjae99@gmail.com>
2024-07-11 11:01:49 +0900

    cleanup: tailwind.config.cjs 필요없는 주석 제거

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -42,19 +42,3 @@ module.exports = {
   },
   plugins: [],
 };
-// module.exports = {
-//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {
-//       colors: {
-
-//         "gray": "#FAFAFA",
-//         "gray-100": "#F5F5F5",
-//         "gray-200": "#F0F0F0",
-
-//       },
-
-//     },
-//   },
-//   plugins: [],
-// };
