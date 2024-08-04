ea2b72cbd216ffdabe3682261f19d9d5f03c0877
HY219 <jhy_0219@daum.net>
2024-08-04 21:56:11 +0900

    refactor: 기존 구현한 로그인 ui와 로그인 기능 연결

---
+++ src/components/atoms/main/header/LoginModal.tsx
@@ -0,0 +1,87 @@
+import { ChangeEvent, FormEvent } from "react";
+import {
+  Backdrop,
+  CloseButton,
+  LoginButton,
+  LoginInput,
+  LoginInputForm,
+  LoginModalBox,
+  LoginModalLogoImage,
+  LoginTitleBox,
+  LoginTitleSpan,
+  ModalContainer,
+} from "./LoginModalStyles";
+import MainLogo from "~/assets/images/homepage_logo_top.svg";
+
+interface Props {
+  onClickToggleModal: () => void;
+  handleEnterUserId: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleEnterPassword: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleClickLogin: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const LoginModal = ({
+  onClickToggleModal,
+  handleEnterUserId,
+  handleEnterPassword,
+  handleClickLogin,
+}: Props) => {
+  return (
+    <ModalContainer>
+      <LoginModalBox>
+        <CloseButton onClick={onClickToggleModal}>×</CloseButton>
+        <LoginTitleBox>
+          <LoginModalLogoImage src={MainLogo} alt="Main Logo" />
+          <LoginTitleSpan>로그인하기</LoginTitleSpan>
+        </LoginTitleBox>
+        <LoginInputForm id="login" onSubmit={handleClickLogin}>
+          <LoginInput type="text" placeholder="ID" onChange={handleEnterUserId} required />
+          <LoginInput
+            type="password"
+            placeholder="PASSWORD"
+            onChange={handleEnterPassword}
+            required
+          />
+          <LoginButton form="login" type="submit">
+            로그인
+          </LoginButton>
+        </LoginInputForm>
+      </LoginModalBox>
+      <Backdrop
+        onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+          if (onClickToggleModal) {
+            onClickToggleModal();
+          }
+        }}
+      />
+    </ModalContainer>
+  );
+};
+
+export default LoginModal;
+// <!-- =======
+// import { ChangeEvent, FormEvent } from "react";
+// import { LoginButton, LoginForm, LoginInput, LoginLayout } from "./LoginStyles";
+
+// interface Props {
+//   handleEnterUserId: (event: ChangeEvent<HTMLInputElement>) => void;
+//   handleEnterPassword: (event: ChangeEvent<HTMLInputElement>) => void;
+//   handleClickLogin: (event: FormEvent<HTMLFormElement>) => void;
+// }
+
+// const Login = ({ handleEnterUserId, handleEnterPassword, handleClickLogin }: Props) => {
+//   return (
+//     <LoginLayout>
+//       <LoginForm id="login" onSubmit={handleClickLogin}>
+//         <LoginInput placeholder="ID" onChange={handleEnterUserId} required />
+//         <LoginInput placeholder="PASSWORD" onChange={handleEnterPassword} required />
+//         <LoginButton form="login" type="submit">
+//           로그인
+//         </LoginButton>
+//       </LoginForm>
+//     </LoginLayout>
+//   );
+// };
+
+// export default Login; -->
--- src/components/molecules/main/header/Accounts.tsx
+++ src/components/molecules/main/header/Accounts.tsx
@@ -1,23 +1,50 @@
+import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 import Account from "~/components/atoms/main/header/Account";
 import logInOut from "~/assets/images/loginout.svg";
 import signUp from "~/assets/images/singup.svg";
+import LoginModal from "~/components/atoms/main/header/LoginModal";
+import usePostLogin from "~/hooks/login/mutate/usePostLogin";
 import { AccountsBox, AccountsWrap } from "./AccountsStyles";
-import Modal from "~/components/templates/login/Login";
-import { useCallback, useState } from "react";
 
 const Accounts = () => {
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  const postLogin = usePostLogin();
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
 
   const onClickToggleModal = useCallback(() => {
     setOpenModal(!isOpenModal);
   }, [isOpenModal]);
+
+  const handleEnterUserId = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setUserId(value);
+  };
+
+  const handleEnterPassword = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setPassword(value);
+  };
+
+  const handleClickLogin = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    postLogin.mutate({
+      userId: userId,
+      password: password,
+    });
+  };
 
   return (
     <AccountsBox>
       <AccountsWrap>
         <Account src={logInOut} alt="Log inout" text="LOG IN" onClick={onClickToggleModal} />
         {isOpenModal && (
-          <Modal onClickToggleModal={onClickToggleModal}>이곳에 children이 들어갑니다.</Modal>
+          <LoginModal
+            onClickToggleModal={onClickToggleModal}
+            handleEnterUserId={handleEnterUserId}
+            handleEnterPassword={handleEnterPassword}
+            handleClickLogin={handleClickLogin}></LoginModal>
         )}
         <Account src={signUp} alt="Sign up" text="SIGN UP" onClick={onClickToggleModal} />
       </AccountsWrap>
