45d6dad32f9f91be335442ad6ca8f29d44601cce
kimmjae99 <kimmjae99@gmail.com>
2024-08-07 16:07:15 +0900

    refactor: boardlist 형식이 변경됨에 따라 쓰이는 case 변경

--- src/components/atoms/news/news/DropDownButton.tsx
+++ src/components/atoms/news/news/DropDownButton.tsx
@@ -8,8 +8,8 @@ interface Props {
 const DropdownButton = ({ value, onChange }: Props) => (
   <DropdownButtonSelect value={value} onChange={onChange}>
     <option value="title">제목</option>
-    <option value="titleAndBody">제목+본문</option>
-    <option value="name">작성자</option>
+    <option value="titleAndContent">제목+본문</option>
+    <option value="owner">작성자</option>
   </DropdownButtonSelect>
 );
 
