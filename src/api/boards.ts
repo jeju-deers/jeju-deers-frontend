effccaa9e8e5232ebfd661fb0f811ae3a6a5b11b
kimmjae99 <kimmjae99@gmail.com>
2024-08-15 16:23:00 +0900

    refactor: boards.ts API에서 board interface 분리

--- src/api/boards.ts
+++ src/api/boards.ts
@@ -1,18 +1,5 @@
 import { instance } from "~/api";
 
-interface Board {
-  id: number;
-  type: string;
-  title: string;
-  content: string;
-  owner: string;
-  images: [];
-  videos: [];
-  createdAt: string;
-  updatedAt: string;
-  views: number;
-}
-
 export const GetBoards = async (type: string) => {
   const { data } = await instance.get("/boards");
   console.log(data);
---
+++ src/api/types/board.ts
@@ -0,0 +1,12 @@
+interface Board {
+  id: number;
+  type: string;
+  title: string;
+  content: string;
+  owner: string;
+  images: [];
+  videos: [];
+  createdAt: string;
+  updatedAt: string;
+  views: number;
+}
