f505a9cc5076cfd63e7a29b82728c7c28c9098bc
kimmjae99 <kimmjae99@gmail.com>
2024-08-16 15:00:01 +0900

    refactor: board 타입 import

--- src/api/boards.ts
+++ src/api/boards.ts
@@ -1,5 +1,5 @@
 import { instance } from "~/api";
-import Board from "./types/board";
+import { Board } from "./types/board";
 
 export const GetBoards = async (type: string) => {
   const { data } = await instance.get("/boards");
