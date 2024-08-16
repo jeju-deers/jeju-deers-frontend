0b8000a6b6b8c93ca48c399c2479c4bf787b4b5f
kimmjae99 <kimmjae99@gmail.com>
2024-08-16 14:53:31 +0900

    feat: 후위연산자를 사용하지 않고 currentId 표현

--- src/components/atoms/news/news/Board.tsx
+++ src/components/atoms/news/news/Board.tsx
@@ -17,7 +17,6 @@ interface Props {
 const Board = ({ boardlist }: Props) => {
   const totalRows = 15;
   const emptyRows = totalRows - boardlist.length;
-  let currentId = 1;
 
   const formatDate = (date: string) => {
     return date.split("T")[0];
@@ -32,15 +31,18 @@ const Board = ({ boardlist }: Props) => {
         <BoardItemBox>등록일</BoardItemBox>
         <BoardItemBox>조회</BoardItemBox>
       </BoardHeaderBox>
-      {boardlist.map(({ id, title, owner, createdAt, views }) => (
-        <BoardRowBox key={id}>
-          <BoardItemBox>{currentId++}</BoardItemBox>
-          <BoardItemBox>{title}</BoardItemBox>
-          <BoardItemBox>{owner}</BoardItemBox>
-          <BoardItemBox>{formatDate(createdAt)}</BoardItemBox>
-          <BoardItemBox>{views}</BoardItemBox>
-        </BoardRowBox>
-      ))}
+      {boardlist.map(({ id, title, owner, createdAt, views }, index) => {
+        let currentId = index + 1;
+        return (
+          <BoardRowBox key={id}>
+            <BoardItemBox>{currentId}</BoardItemBox>
+            <BoardItemBox>{title}</BoardItemBox>
+            <BoardItemBox>{owner}</BoardItemBox>
+            <BoardItemBox>{formatDate(createdAt)}</BoardItemBox>
+            <BoardItemBox>{views}</BoardItemBox>
+          </BoardRowBox>
+        );
+      })}
       {[...Array(emptyRows)].map((_, index) => (
         <BoardRowBox key={`${index}-empty`}>
           <BoardItemBox>&nbsp;</BoardItemBox>
