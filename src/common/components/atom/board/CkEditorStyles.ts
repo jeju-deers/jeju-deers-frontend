da27f3937cf78aa65d0c0fbfcc0e1f7455c3a3d2
kimmjae99 <kimmjae99@gmail.com>
2024-08-26 13:09:07 +0900

    style: CkEditor 설치 및 설정

---
+++ src/common/components/atom/board/CkEditorStyles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+export const MainContainer = styled.div`
+  ${tw`
+    font-sans
+  `}
+  font-family: 'Lato', sans-serif;
+`;
+
+export const EditorContainer = styled.div`
+  ${tw`
+    flex 
+    flex-col 
+    
+  `}
+`;
+
+export const CKEditorWrapper = styled.div`
+  ${tw`
+    
+  `}
+
+  .ck-editor__editable {
+    ${tw`min-h-96`}
+  }
+
+  .media {
+    ${tw`
+        w-[400px]
+    `}
+  }
+`;
