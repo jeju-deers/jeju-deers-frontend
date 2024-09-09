c98142c4f38a77844ba9e19fa179acc75e8936cc
HY219 <jhy_0219@daum.net>
2024-09-09 13:45:38 +0900

    style: 스타일 없이 전체 구조 div 영역 설정

--- src/common/components/templates/boardDetail/BoardDetail.tsx
+++ src/common/components/templates/boardDetail/BoardDetail.tsx
@@ -1,5 +1,57 @@
 const BoardDetail = () => {
-  return <>게시물 상세 페이지 입니다.</>;
+  return (
+    <>
+      <div>
+        <div>이전글</div>
+        <div>다음글</div>
+        <div>등록</div>
+      </div>
+      <div>
+        <div>디어스 훈련 일정</div>
+        <div>24년 8월 훈련 일정 공지</div>
+        <div>
+          <div>
+            <div>
+              <div>박재광</div>
+              <div>매니저</div>
+            </div>
+            <div>2024년 8월 23일 13:30</div>
+          </div>
+          <div>
+            <div>
+              <div>조회수</div>
+              <div>0</div>
+            </div>
+            <div>
+              <div>댓글수</div>
+              <div>10</div>
+            </div>
+          </div>
+        </div>
+        <div>제 이름은 박재광 입니다.</div>
+        <div>댓글</div>
+        <div>
+          <div>
+            <div>김민재</div>
+            <div>OB</div>
+          </div>
+          <div>확실한 가요?</div>
+          <div>2024.08.23. 14:00</div>
+        </div>
+        <div>
+          <div>
+            <div>박재광</div>
+            <div>댓글을 남겨보세요</div>
+          </div>
+          <div>등록</div>
+        </div>
+      </div>
+      <div>
+        <div>목록</div>
+        <div>TOP</div>
+      </div>
+    </>
+  );
 };
 
 export default BoardDetail;
