2af8bd98ce893ad497f5e35b8cb1c14a49bbbcee
HY219 <jhy_0219@daum.net>
2024-09-09 11:03:50 +0900

    modify: 캐시 초기화를 이용해 마이페이지에 항상 최신 회원정보 불러오도록 구현
    react-query의 useQueryClient를 이용해 'useInformation' 쿼리를 무효화하고 다시 데이터를 가져옵니다.

--- src/hooks/myPage/mutate/useUpdateUserInformation.ts
+++ src/hooks/myPage/mutate/useUpdateUserInformation.ts
@@ -1,9 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { GetUserInformationProps } from "~/api/types/users";
 import { putUserInformation } from "~/api/users";
 
 const useUpdateUserInformation = (userId: string | null, token: string | null) => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const objectInformation = {
@@ -17,6 +18,7 @@ const useUpdateUserInformation = (userId: string | null, token: string | null) =
     },
     onSuccess: () => {
       console.log(`회원정보 수정 성공`);
+      queryClient.invalidateQueries({ queryKey: ["userInformation"] });
       alert("회원정보가 수정되었습니다.");
       navigate("/");
     },
