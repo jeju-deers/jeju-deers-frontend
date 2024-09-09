d23a76cd772885365d0549a7cd652d31a44c771e
HY219 <jhy_0219@daum.net>
2024-09-09 17:58:37 +0900

    cleanup: styles 파일 줄간격 일정하도록 수정

--- src/common/components/atom/boardDetail/ButtonStyles.tsx
+++ src/common/components/atom/boardDetail/ButtonStyles.tsx
@@ -7,32 +7,32 @@ interface ButtonProps {
 
 export const ButtonButton = styled.button<ButtonProps>`
   ${tw`
-      flex
-      h-14
-      text-2xl
-      rounded-lg-xl
-      items-center
-      justify-center
-    `}
+    flex
+    h-14
+    text-2xl
+    rounded-lg-xl
+    items-center
+    justify-center
+  `}
   ${({ styleType }) =>
     styleType === "movePostButton" &&
     tw`
-      w-33
-      border
-      border-gray-500
-    `}
+    w-33
+    border
+    border-gray-500
+  `}
   ${({ styleType }) =>
     styleType === "submitCommentButton" &&
     tw`
-      w-25
-      bg-blue
-      text-white
-    `}
+    w-25
+    bg-blue
+    text-white
+  `}
   ${({ styleType }) =>
     styleType === "moveListButton" &&
     tw`
-      w-28.25
-      border
-      border-gray-500
-    `}
+    w-28.25
+    border
+    border-gray-500
+  `}
 `;
--- src/common/components/templates/boardDetail/BoardDetailStyles.tsx
+++ src/common/components/templates/boardDetail/BoardDetailStyles.tsx
@@ -3,27 +3,27 @@ import styled from "styled-components";
 
 export const BoardDetailLayout = styled.div`
   ${tw`
-      w-full
-      px-32
-      py-20
-      min-w-250
-    `}
+    w-full
+    px-32
+    py-20
+    min-w-250
+  `}
 `;
 
 export const MovePostButtonBox = styled.div`
   ${tw`
-      flex
-      gap-3
-      w-full
-      justify-end
-    `}
+    flex
+    gap-3
+    w-full
+    justify-end
+  `}
 `;
 
 export const MoveListButtonBox = styled.div`
   ${tw`
-      flex
-      gap-5
-      w-full
-      justify-end
-    `}
+    flex
+    gap-5
+    w-full
+    justify-end
+  `}
 `;
