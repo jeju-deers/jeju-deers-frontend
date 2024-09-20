7b63097b9e39e1b7eecdf8a85717bac650e1b646
kimmjae99 <kimmjae99@gmail.com>
2024-09-20 19:36:24 +0900

    feat: 글쓰기 기능 구현

--- src/common/components/templates/NewsWrite.tsx
+++ src/common/components/templates/NewsWrite.tsx
@@ -13,9 +13,9 @@ import {
 import { NOTICE_DATA } from "~/constants/noticeData";
 import WriteDropdownButton from "../atom/board/WriteDropDownButton";
 import CkEditor from "../atom/board/CkEditor";
-import axios from "axios";
 import Heading from "~/components/atoms/club/Heading";
 import helmet from "~/assets/images/helmet.svg";
+import usePostWrite from "~/common/hooks/write/mutate/usePostWrite";
 
 interface Props {
   writeOption: string;
@@ -25,32 +25,23 @@ interface Props {
 const NewsWrite = ({ writeOption, setWriteOption }: Props) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const { mutate, isError, isSuccess } = usePostWrite();
 
   const handleWriteDropdownChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setWriteOption(e.target.value);
   };
 
-  const handleSave = async () => {
+  const handleSave = () => {
     const token = localStorage.getItem("token");
-    try {
-      const response = await axios.post(
-        "https://jeju-deers-backend.fly.dev/boards",
-        {
-          title,
-          content,
-          type: writeOption,
-          owner: "kimmjae2312",
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      console.log("Post saved successfully:", response.data);
-    } catch (error) {
-      console.error("Error saving the post:", error);
+    const owner = "abc";
+
+    if (!token) {
+      console.error("토큰이 만료되었습니다. 다시 로그인 해주세요");
+      return;
     }
+    const upperCaseWriteOption = writeOption.toUpperCase();
+    mutate({ title, content, owner, type: upperCaseWriteOption, token });
+    console.log(title, content, owner, upperCaseWriteOption);
   };
 
   return (
@@ -59,6 +50,8 @@ const NewsWrite = ({ writeOption, setWriteOption }: Props) => {
       <SaveButtonWrap>
         <SaveButton onClick={handleSave}>등록</SaveButton>
       </SaveButtonWrap>
+      {isError && <p style={{ color: "red" }}>게시글 저장 실패!</p>}
+      {isSuccess && <p style={{ color: "green" }}>게시글이 성공적으로 저장되었습니다!</p>}
       <NoticeBox>
         <Notice>{NOTICE_DATA}</Notice>
       </NoticeBox>
