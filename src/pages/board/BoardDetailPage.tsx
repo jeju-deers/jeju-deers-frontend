8dd9346ffe6c4c9ef60ab396639ce8c9767ab757
HY219 <jhy_0219@daum.net>
2024-10-26 23:11:37 +0900

    comment: 조회할 게시글의 아이디 임의 적용

--- src/pages/board/BoardDetailPage.tsx
+++ src/pages/board/BoardDetailPage.tsx
@@ -1,7 +1,8 @@
 import BoardDetail from "~/common/components/templates/boardDetail/BoardDetail";
 
 const BoardDetailPage = () => {
-  const singleBoardId = "15";
+  // TODO: [2024-10-26] 조회할 게시글의 아이디를 임의로 적용. 추후 수정 필요.
+  const singleBoardId = "24";
 
   return <BoardDetail singleBoardId={singleBoardId} />;
 };
