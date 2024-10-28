cd942d84c9ea6b51545046c87a272fd106222686
HY219 <jhy_0219@daum.net>
2024-10-28 18:00:06 +0900

    modify: 게시글 작성 시간이 올바르게 출력되도록 수정 &  월, 일, 시, 분을 항상 두자리로 출력
    한 자리일 경우, 앞에 0을 붙여 두자리로 출력

--- src/hooks/board/useFormatKoreanTime.ts
+++ src/hooks/board/useFormatKoreanTime.ts
@@ -1,11 +1,11 @@
 const useFormatKoreanTime = (utcDate: string) => {
   const date = new Date(utcDate);
-  const koreanTime = new Date(date.getTime() + 9 * 60 * 1000);
+  const koreanTime = new Date(date.getTime() + 9 * 60 * 60 * 1000);
 
   const year = koreanTime.getFullYear();
-  const month = koreanTime.getMonth() + 1;
-  const day = koreanTime.getDate();
-  const hours = koreanTime.getHours();
+  const month = (koreanTime.getMonth() + 1).toString().padStart(2, "0");
+  const day = koreanTime.getDate().toString().padStart(2, "0");
+  const hours = koreanTime.getHours().toString().padStart(2, "0");
   const minutes = koreanTime.getMinutes().toString().padStart(2, "0");
 
   return { year, month, day, hours, minutes };
