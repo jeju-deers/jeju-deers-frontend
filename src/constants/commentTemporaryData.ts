f7fa5b71028783dfcd52ec8c772998c8106bd7cf
Hyeyeon Jeong <jhy_0219@daum.net>
2024-11-11 18:36:23 +0900

    [#56] feat: 게시글 상세페이지 내 댓글 조회 기능 구현 (#57)
    * style: 게시물 카테고리에 해당하는 header 출력
    
    임시로, 연습 일정 게시판 상세 페이지로 UI 구현
    
    * style: 스타일 없이 전체 구조 div 영역 설정
    
    * style: 페이지 최상단 레이아웃의 전체 영역및 button에 대한 style 지정 및 적용
    
    최상단 레이아웃의 전체 영역에 대한 with와 여백 설정
    페이지에서 사용되는 모든 button에 대한 스타일을 한 컴포넌트를 이용하여 지정
    styleType을 props로 전달하여 각 버튼에 알맞은 style을 지정할 수 있도록 구현
    
    * style: 오른쪽 화살표 svg 이미지 추가
    
    nextArrowImage
    
    * cleanup: styles 파일 줄간격 일정하도록 수정
    
    * style: text 스타일 지정 및 적용
    
    className을 사용하여 스타일 적용
    
    좀 더 통일성있는 방법을 사용할 수 있는지 고민 필요
    
    * style: 게시물 상세 페이지 UI 구현
    
    * 조회수, 댓글수 스타일 수정 및 등록 버튼 제거
    
    tailwind.config에 스타일 등록
    
    * feat: 댓글 목록 임시 데이터 생성
    
    * feat: 댓글 조회 기능 구현
    
    임시 댓글 목록 상수 데이터 이용
    map함수를 이용하여 출력
    첫 댓글 이후 댓글들은 구분선 출력

---
+++ src/constants/commentTemporaryData.ts
@@ -0,0 +1,16 @@
+const COMMENT_TEMPORORY_DATA = [
+  {
+    name: "김민재",
+    belong: "OB",
+    create_at: "2024.10.27 12:29",
+    content: "확실한가요?",
+  },
+  {
+    name: "고겨래",
+    belong: "OB",
+    create_at: "2024.10.27 12:30",
+    content: "지가 봤구먼유",
+  },
+];
+
+export default COMMENT_TEMPORORY_DATA;
