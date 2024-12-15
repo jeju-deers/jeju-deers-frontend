42dd2c1e1b6db8c1a68a8711e802b07cdad146ed
HY219 <jhy_0219@daum.net>
2024-12-15 13:50:08 +0900

    rename: text 오타 수정

--- src/constants/userInformationData.ts
+++ src/constants/userInformationData.ts
@@ -7,14 +7,14 @@ const USER_INFORMATION_DATA = [
     createAt: "2024.08.23. 14:10",
   },
   {
-    name: "고겨래",
+    name: "고겨레",
     belong: "YB",
     userType: "코치",
     authority: "관리자",
     createAt: "2024.08.23. 14:10",
   },
   {
-    name: "고겨래",
+    name: "고겨레",
     belong: "YB",
     userType: "스탭",
     authority: "외부 회원",
