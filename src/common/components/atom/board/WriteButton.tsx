5734b36968633c92f88b4ab3fcf5515649e835a3
kimmjae99 <kimmjae99@gmail.com>
2024-12-28 19:21:56 +0900

    refactor: 필요없는 react-router-dom 기능 제거

--- src/common/components/atom/board/WriteButton.tsx
+++ src/common/components/atom/board/WriteButton.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { WriteButtonButton } from "./WriteButtonStyles";
 
 const WriteButton = () => {
