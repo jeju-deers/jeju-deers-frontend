0d1a3d0f2196b33600aa2b83bc989b424799be94
kimmjae99 <kimmjae99@gmail.com>
2024-12-28 19:21:00 +0900

    refactor: 조건에 따른 WriteButton 렌더링 수정

--- src/common/components/atom/board/WriteButton.tsx
+++ src/common/components/atom/board/WriteButton.tsx
@@ -2,32 +2,28 @@ import { Link, useParams } from "react-router-dom";
 import { WriteButtonButton } from "./WriteButtonStyles";
 
 const WriteButton = () => {
-  const handleClickWriteButton = () => {
-    const token = localStorage.getItem("token");
-    const { section } = useParams();
-    const userType = localStorage.getItem("userType");
+  const section = location.pathname.split("/").filter(Boolean).pop();
+  const token = localStorage.getItem("token");
+  const userType = localStorage.getItem("userType");
 
-    const canWrite = (() => {
-      if (!token) return false;
-      if (userType === "player" && (section === "coach_board" || "staff_board")) {
-        return false;
-      }
-      if (userType === "external" && section !== "guest_board") {
-        return false;
-      }
-      return true;
-    })();
-
-    if (!canWrite) {
-      return null;
+  const canWrite = (() => {
+    if (!token) return false;
+    if (userType === "player" && (section === "coach_board" || section === "staff_board")) {
+      return false;
     }
-  };
+    if (userType === "external" && section !== "guest_board") {
+      return false;
+    }
+    return true;
+  })();
+
+  if (!canWrite) {
+    return null;
+  }
 
   return (
     <WriteButtonButton>
-      <Link to="./write" onClick={handleClickWriteButton}>
-        글쓰기
-      </Link>
+      <Link to="./write">글쓰기</Link>
     </WriteButtonButton>
   );
 };
