0dc1d768f26198ef18b8d721cddc1cd3e1fc0429
kimmjae99 <kimmjae99@gmail.com>
2024-12-28 19:02:58 +0900

    refactor: 조건에 따른 WriteButton 렌더링

--- src/common/components/atom/board/WriteButton.tsx
+++ src/common/components/atom/board/WriteButton.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { WriteButtonButton } from "./WriteButtonStyles";
 
 const WriteButton = () => {
-  const handleClickWriteButton = (event: React.MouseEvent) => {
+  const handleClickWriteButton = () => {
     const token = localStorage.getItem("token");
+    const { section } = useParams();
+    const userType = localStorage.getItem("userType");
 
-    if (!token) {
-      event.preventDefault();
-      alert("로그인 이후 가능한 기능입니다.");
-      return;
+    const canWrite = (() => {
+      if (!token) return false;
+      if (userType === "player" && (section === "coach_board" || "staff_board")) {
+        return false;
+      }
+      if (userType === "external" && section !== "guest_board") {
+        return false;
+      }
+      return true;
+    })();
+
+    if (!canWrite) {
+      return null;
     }
   };
 
