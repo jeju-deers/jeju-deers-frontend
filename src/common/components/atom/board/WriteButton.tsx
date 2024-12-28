d4a5ccacfcac0047254ebb5a4f6b4588c39a4566
kimmjae99 <kimmjae99@gmail.com>
2024-12-28 18:42:37 +0900

    refactor: handle함수 명칭 변경

--- src/common/components/atom/board/WriteButton.tsx
+++ src/common/components/atom/board/WriteButton.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import { WriteButtonButton } from "./WriteButtonStyles";
 
 const WriteButton = () => {
-  const handleClickButton = (e: React.MouseEvent) => {
+  const handleClickWriteButton = (event: React.MouseEvent) => {
     const token = localStorage.getItem("token");
 
     if (!token) {
-      e.preventDefault();
+      event.preventDefault();
       alert("로그인 이후 가능한 기능입니다.");
       return;
     }
@@ -14,7 +14,7 @@ const WriteButton = () => {
 
   return (
     <WriteButtonButton>
-      <Link to="./write" onClick={handleClickButton}>
+      <Link to="./write" onClick={handleClickWriteButton}>
         글쓰기
       </Link>
     </WriteButtonButton>
