8403b309449db17d8c6985aef8423b14d700f803
HY219 <jhy_0219@daum.net>
2025-01-02 12:27:32 +0900

    refactor: 중복 header 제거
    Outlet header를 이용
    기존 header 제거

--- src/pages/board/BoardDetailPage.tsx
+++ src/pages/board/BoardDetailPage.tsx
@@ -2,7 +2,7 @@ import BoardDetail from "~/common/components/templates/boardDetail/BoardDetail";
 
 const BoardDetailPage = () => {
   // TODO: [2024-10-26] 조회할 게시글의 아이디를 임의로 적용. 추후 수정 필요.
-  const singleBoardId = "26";
+  const singleBoardId = "65";
 
   return <BoardDetail singleBoardId={singleBoardId} />;
 };
