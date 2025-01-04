347fb6f08b61aa2564e6cb754d4ed2749c10f32c
kimmjae99 <kimmjae99@gmail.com>
2025-01-04 15:07:06 +0900

    feat: 업데이트, 삭제의 API 및 hook 구현

--- src/api/boards.ts
+++ src/api/boards.ts
@@ -3,5 +3,56 @@ import { Board } from "./types/board";
 
 export const GetBoards = async (type: string) => {
   const { data } = await instance.get("/boards");
-  return data.filter((board: Board) => board.type === type);
+  return data
+    .filter((board: Board) => board.type === type)
+    .sort(
+      (a: Board, b: Board) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    );
+};
+
+export const DeleteBoard = async (id: string, token: string | null) => {
+  return await instance.delete(`/boards/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+interface UpdateBoardProps {
+  id: string;
+  title: string;
+  content: string;
+  type: string;
+  owner: string;
+  token: string;
+}
+
+export const UpdateBoard = async ({ id, title, content, type, token, owner }: UpdateBoardProps) => {
+  const response = await instance.put(
+    `/boards/${id}`,
+    { title, content, type, owner },
+    { headers: { Authorization: `Bearer ${token}` } },
+  );
+  console.log(title);
+  console.log(owner);
+  console.log(type);
+  console.log(content);
+  return response.data;
+};
+
+interface GetBoardDetailsProps {
+  id: number;
+  token: string;
+}
+
+export const getBoardDetails = async ({ id, token }: GetBoardDetailsProps) => {
+  try {
+    const response = await instance.get(`/boards/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching board details: ${error}`);
+    throw error;
+  }
 };
---
+++ src/hooks/board/mutate/useDeleteBoard.ts
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { DeleteBoard } from "~/api/boards";
+
+interface Props {
+  id: string;
+  token: string;
+}
+
+const useDeleteBoard = () => {
+  const mutation = useMutation({
+    mutationFn: ({ id, token }: Props) => {
+      return DeleteBoard(id, token);
+    },
+    onSuccess: () => {
+      console.log("게시물이 삭제되었습니다.");
+    },
+    onError: (error: Error) => {
+      console.error(`게시물 삭제에 실패했습니다: ${error}`);
+    },
+  });
+  return mutation;
+};
+
+export default useDeleteBoard;
