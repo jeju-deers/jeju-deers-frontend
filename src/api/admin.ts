7e112778c64abcc322965205bfb72ec0490f14fe
HY219 <jhy_0219@daum.net>
2025-01-11 07:54:35 +0900

    feat: 관리자의 사용자 삭제 API 연결
    삭제할 사용자 목록 id를 요청 body에 담기 위해, data를 사용

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -3,3 +3,7 @@ import { instance } from ".";
 export const getUserList = async () => {
   return await instance.get("/admin/dashboard");
 };
+
+export const deleteUserList = async (ids: string[]) => {
+  return await instance.delete("/admin/users", { data: { ids } });
+};
