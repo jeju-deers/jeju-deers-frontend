b18905600141a1d682137a064407e96bb0944fdc
HY219 <jhy_0219@daum.net>
2025-01-11 00:45:05 +0900

    feat: 403에러 발생 시, 페이지 권한 없음 로그인 필요 alert창 출력
    axios, AxiosResponse 이용
    응답 상태 코드가 403일 경우 alert 출력

--- src/hooks/comment/mutate/usePostComment.ts
+++ src/hooks/comment/mutate/usePostComment.ts
@@ -1,4 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import { postComment } from "~/api/comment";
 
 interface PostCommentProps {
@@ -16,7 +17,19 @@ const usePostComment = () => {
     onSuccess: () => {
       console.log(`댓글 작성 성공`);
     },
-    onError: (error) => console.log(`댓글 작성 실패, error: ${error}`),
+    onError: (error: Error) => {
+      console.log(`댓글 작성 실패, error: ${error}`);
+
+      if (!axios.isAxiosError(error)) {
+        return;
+      }
+
+      const statusCode = error.response?.status;
+
+      if (statusCode === 403) {
+        alert("페이지 권한이 없습니다. 로그인 후 이용해주세요.");
+      }
+    },
   });
   return mutation;
 };
