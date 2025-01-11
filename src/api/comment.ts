63a44864d8f0c3e00b0e769ceb7fb832dd591f80
Hyeyeon Jeong <jhy_0219@daum.net>
2025-01-11 14:38:11 +0900

    [#85] feat: 게시판 내 댓글 작성 API 연결 (#86)
    * style: 댓글 작성 textarea UI 구현
    
    여러줄을 작성하고, 자동 줄바꿈이 가능하도록 input 보다 textarea를 사용하는 것이 적합하다고 판단
    outline-none : textarea 클릭 시, 자동 생성 outline 제거
    resize-none : 자동 생성 textarea 크기 조절 제거
    
    * feat: 댓글 작성자에 로그인한 유저의 이름 출력
    
    * feat: 댓글 작성 기능 구현
    
    onChange, useState 이용
    comment 상태값 이용
    참고 : useState 변수 선언과 isLoading 의 코드 순서에 유의해야합니다. (rendering 관련)
    
    * feat: 댓글이 빈 글일 때 alert 창 출력
    
    * feat: 댓글 작성 api 연결
    
    현재 500에러 반환
    백엔드 작업 후 재확인 필요
    
    * feat: 403에러 발생 시, 페이지 권한 없음 로그인 필요 alert창 출력
    
    axios, AxiosResponse 이용
    응답 상태 코드가 403일 경우 alert 출력

---
+++ src/api/comment.ts
@@ -0,0 +1,21 @@
+import { instance } from ".";
+
+interface PostCommentProps {
+  postId: string;
+  name: string;
+  belong?: string;
+  content: string;
+  token: string;
+}
+
+// TODO: [2025-01-11] 현재 500에러를 반환합니다. 백엔드에서 확인 후 재확인이 필요합니다.
+export const postComment = async ({ postId, name, belong, content, token }: PostCommentProps) => {
+  const { data } = await instance.post(
+    `/comments/${postId}`,
+    { name, belong, content },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    },
+  );
+  return data;
+};
