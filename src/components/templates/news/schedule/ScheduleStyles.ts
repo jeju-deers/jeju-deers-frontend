d0111f2daac75e845953e4bb64458cd9fe33eb54
kimmjae99 <kimmjae99@gmail.com>
2025-01-18 12:26:11 +0900

    style: Schedule 컴포넌트 UI 구현

--- src/components/templates/news/schedule/ScheduleStyles.ts
+++ src/components/templates/news/schedule/ScheduleStyles.ts
@@ -7,5 +7,26 @@ export const ScheduleLayout = styled.div`
     w-full
     py-20
     px-28
+    flex-row
+    text-center
+  `}
+`;
+
+export const AddButton = styled.button`
+  ${tw`
+    mt-10
+    py-3
+    px-8
+    text-xl
+    text-white
+    rounded-lg
+    border
+    border-gray-300
+    bg-green-dark
+    cursor-pointer
+    hover:bg-gray-300
+    focus:outline-none
+    focus:ring-2
+    focus:ring-gray-400
   `}
 `;
