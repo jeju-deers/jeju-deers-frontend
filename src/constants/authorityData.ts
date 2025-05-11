398e8f102380352f3326c62e84cb0ed97f77a314
HY219 <jhy_0219@daum.net>
2025-05-11 16:18:04 +0900

    refactor: 소속, 역할, 권한 Dropdown에 전체 text 추가

--- src/constants/authorityData.ts
+++ src/constants/authorityData.ts
@@ -1,3 +1,3 @@
-const AUTHORITY_DATA = ["일반 회원", "관리자", "외부 회원"];
+const AUTHORITY_DATA = ["일반 회원", "관리자", "외부 회원", "전체"];
 
 export default AUTHORITY_DATA;
--- src/constants/userTypeData.ts
+++ src/constants/userTypeData.ts
@@ -1,3 +1,3 @@
-const USER_TYPE_DATA = ["선수", "코치", "스탭", "외부인"];
+const USER_TYPE_DATA = ["선수", "코치", "스탭", "외부인", "전체"];
 
 export default USER_TYPE_DATA;
