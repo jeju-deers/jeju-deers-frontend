db26cc8d3e0f761e844c35158a2e778147066d18
HY219 <jhy_0219@daum.net>
2025-05-11 16:03:37 +0900

    rename: 사용자 삭제 api에 대한 매개변수 ids를 useIds로 변경
    사용자 수정 api에 대한 매개변수명과의 통일성을 위해 변경

--- src/hooks/admin/query/mutate/useDeleteUserList.ts
+++ src/hooks/admin/query/mutate/useDeleteUserList.ts
@@ -5,7 +5,7 @@ const useDeleteUserList = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
-    mutationFn: (ids: string[]) => deleteUserList(ids),
+    mutationFn: (userIds: string[]) => deleteUserList(userIds),
     onSuccess: () => {
       console.log("사용자가 성공적으로 삭제되었습니다.");
       alert("사용자가 성공적으로 삭제되었습니다.");
